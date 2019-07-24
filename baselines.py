93c4add5183bb5cabee24d05e8a77088fe48cf2a
Alana Marzoev <alanamarzoev@gmail.com>
2019-07-23 17:06:27 -1000

    add missing defaults

--- baselines.py
+++ baselines.py
@@ -16,8 +16,8 @@ chdir = "git checkout -f lstm-embeddings"
 os.system(chdir)
 
 # 2. modified spatial-reasoning experiments for local and global human and synthetic annotations 
-exp3 = "python2 reinforcement.py --annotations human --mode local --save_path ../exp_results/lstm_embeddings_human_local"
-exp4 = "python2 reinforcement.py --annotations human --mode global --save_path ../exp_results/lstm_embeddings_human_global"
+exp3 = "python2 reinforcement.py --annotations human --mode local --save_path ../exp_results/lstm_embeddings_human_local --max_train 5000 --max_test 500"
+exp4 = "python2 reinforcement.py --annotations human --mode global --save_path ../exp_results/lstm_embeddings_human_global --max_train 5000 --max_test 500"
 
 exp5 = "python2 reinforcement.py --annotations synthetic --mode local --save_path ../exp_results/lstm_embeddings_synthetic_local --max_train 99 --max_test 10"
 exp6 = "python2 reinforcement.py --annotations synthetic --mode global --save_path ../exp_results/lstm_embeddings_synthetic_global --max_train 99 --max_test 10"
